26e8a3bd07f54d741a0a83f7a04d3796e553c1a7
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-08-10 16:02:45 +0500

    init

--- src/pages/Dashboard/ItemsForProfile/TweetsReplies.jsx
+++ src/pages/Dashboard/ItemsForProfile/TweetsReplies.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import PostCart from "../../../components/PostCart";
 
-function TweetsReplies() {
+function Tweets() {
+  const token = JSON.parse(window.localStorage.getItem("token"));
+  const usersPost = JSON.parse(window.localStorage.getItem("usersPost"));
   return (
-    <div>TweetsReplies</div>
-  )
+    <div>
+      {usersPost.filter((item) => item.name == token.login && item.replyCount).length ? (
+        usersPost.filter((item) => item.name == token.login && item.replyCount).map(item => <PostCart item={item} key={item.id}/>)
+      ) : (
+        <p className="capitalize font-bold text-2xl flex items-center justify-center mt-8">not yet reply tweets</p>
+      )}
+    </div>
+  );
 }
 
-export default TweetsReplies
+export default Tweets;
